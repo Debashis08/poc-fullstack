4d309f917c0e524fe2c0d113919044d437a86755
Debashis Nandi <debashisnandi576@gmail.com>
2026-06-16 02:27:44 +0530

    feat: main.tf, providers.tf files created

---
+++ poc-azure-functions/infrastructure/terraform/main.tf
@@ -0,0 +1,45 @@
+# 1. Create a Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-fullstack-poc-dev"
+  location = "East US" # Feel free to change to your closest region
+}
+
+# 2. Create a globally unique Storage Account
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = "stfuncpocdev${random_integer.ri.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+# 3. Create the Consumption Plan (Serverless / Free tier friendly)
+resource "azurerm_service_plan" "asp" {
+  name                = "asp-fullstack-poc-dev"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Windows"
+  sku_name            = "Y1" # Y1 is the dynamic Consumption tier
+}
+
+# 4. Create the actual Function App
+resource "azurerm_windows_function_app" "func" {
+  name                       = "func-fullstack-poc-dev" # EXACTLY matches your YAML variable
+  resource_group_name        = azurerm_resource_group.rg.name
+  location                   = azurerm_resource_group.rg.location
+  service_plan_id            = azurerm_service_plan.asp.id
+  storage_account_name       = azurerm_storage_account.sa.name
+  storage_account_access_key = azurerm_storage_account.sa.primary_access_key
+
+  site_config {
+    application_stack {
+      use_dotnet_isolated_runtime = true
+      dotnet_version              = "v8.0" # Azure officially supports v8.0 for isolated workers right now
+    }
+  }
+}
