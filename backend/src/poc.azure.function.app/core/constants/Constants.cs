cd7f41830b9ba1eacec09e9f50b10e3fc3d42705
Debashis Nandi <debashisnandi576@gmail.com>
2026-08-14 00:55:49 +0530

    feat-be: removed old project

--- backend/src/poc.azure.function.app/core/constants/Constants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace poc.azure.function.app;
-
-public static class Constants
-{
-    public readonly static string AZURITE_LOCAL_DEV_QUEUE_NAME = "sbq-dev";
-    public readonly static string AZURITE_LOCAL_TEST_QUEUE_NAME = "sbq-test";
-}
